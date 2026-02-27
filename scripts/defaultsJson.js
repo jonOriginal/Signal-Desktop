e6dd754cc8d35a76c953afb84d17c16dbd1f0086
jonOriginal <jonathan-knox@outlook.com>
2026-02-28 12:31:29 +1300

    Add defaultsJson.js and update custom build script for configuration management

---
+++ scripts/defaultsJson.js
@@ -0,0 +1,12 @@
+﻿const { readFileSync } = require('node:fs');
+const { join } = require('node:path');
+
+const DEFAULTS_JSON_PATH = join(__dirname, '..', 'config', 'defaults.json');
+
+const json = JSON.parse(readFileSync(DEFAULTS_JSON_PATH, 'utf8'));
+
+exports.default = json;
+exports.name = json.name;
+exports.version = json.version;
+exports.productName = json.productName;
+exports.build = json.build;
--- scripts/prepare_custom_build.js
+++ scripts/prepare_custom_build.js
@@ -5,6 +5,7 @@
 const fs = require('node:fs');
 const _ = require('lodash');
 const { default: packageJson } = require('./packageJson.js');
+const { default: defaultsConfig } = require('./defaultsConfig.js');
 
 // ---------------------------
 // Environment variables for prefixes
@@ -12,6 +13,7 @@ const CHANNEL = process.env.SIGNAL_CHANNEL || 'production'; // production, alpha
 const DISPLAY_NAME = process.env.DISPLAY_NAME_PREFIX || 'Signal';
 const APP_NAME = process.env.APP_NAME_PREFIX || 'signal';
 const DOMAIN = process.env.DOMAIN_PREFIX || 'org.whispersystems';
+const UPDATE_URL = process.env.UPDATE_URL || 'https://updates.signal.org/desktop';
 
 // ---------------------------
 // Generate final names
@@ -22,6 +24,7 @@ const config = {
   wmClass: `${APP_NAME}-${CHANNEL}`,
   desktopName: `${APP_NAME}.desktop`,
   executableName: `${APP_NAME}-desktop-${CHANNEL}`,
+  updateURL: `${UPDATE_URL}`,
 };
 
 // ---------------------------
@@ -33,6 +36,12 @@ function updatePackageJson(pkg, cfg) {
   _.set(pkg, 'build.linux.desktop.entry.StartupWMClass', cfg.wmClass);
   _.set(pkg, 'desktopName', cfg.desktopName);
   _.set(pkg, 'build.linux.executableName', cfg.executableName);
+  _.set(pkg, 'build.mac.publish.url', cfg.updateURL);
+  _.set(pkg, 'build.win.publish.url', cfg.updateURL);
+}
+
+function updateDefaultsConfig(def, cfg) {
+  _.set(def, 'updatesUrl', cfg.updateURL);
 }
 
 // ---------------------------
@@ -42,4 +51,11 @@ console.log(JSON.stringify(config, null, 2));
 
 updatePackageJson(packageJson, config);
 fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+
 console.log(`package.json updated for ${CHANNEL} build`);
+
+updateDefaultsConfig(defaultsConfig, config);
+fs.writeFileSync('./config/defaults.js', JSON.stringify(defaultsConfig, null, 2));
+
+console.log(`defaultsConfig.js updated for ${CHANNEL} build`);
+
